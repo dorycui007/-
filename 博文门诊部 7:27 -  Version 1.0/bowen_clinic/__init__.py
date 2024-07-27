4f30534c651423cd7982830a11b9c342ea430254
Ericsson Cui <doryc007@gmail.com>
2024-07-27 20:20:51 +0800

    Update __init__.py
    Removing unused imports

--- 博文门诊部 7:27 -  Version 1.0/bowen_clinic/__init__.py
+++ 博文门诊部 7:27 -  Version 1.0/bowen_clinic/__init__.py
@@ -1,8 +1,4 @@
-import os
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'thisisthesecretkey'
- 
-from bowen_clinic import routes
