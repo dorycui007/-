1ec951651d7f05fb6b705a3af4abf76e1078315a
Ericsson Cui <doryc007@gmail.com>
2024-08-03 08:45:33 +0800

    Delete 博文门诊部 7:27 -  Version 1.0 directory

--- 博文门诊部 7:27 -  Version 1.0/app.py
+++
@@ -1,6 +0,0 @@
-from bowen_clinic import app
-
-if __name__ == '__main__':
-    app.jinja_env.cache = {}
-    # bowen clinic: host="192.168.31.66"
-    app.run(host="192.168.31.66", port=5000, debug=True)
